fab56d141fa77d5f8ac3b8075b42d764f8f2b028
ruben malchow <ruben.malchow@gmail.com>
2014-11-10 17:06:58 +0800

    release prepare

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
+
+git add . && git commit -m "release prepare" && git push
+git checkout master
+git merge develop
+git add . && git commit -m "release prepare" && git push
 mvn --batch-mode -s /Users/rm/Eclipse/m2/fms/settings.xml release:prepare release:perform
+git checkout develop
