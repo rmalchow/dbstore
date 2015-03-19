20d3bd94563e29d7979572c16a6e0ca0804ddaee
ruben malchow <ruben.malchow@gmail.com>
2015-03-19 08:38:15 +0800

    release prepare

--- release.sh
+++ release.sh
@@ -1,8 +1,13 @@
-
+echo release version
+read RV
+echo development version
+read DV
 git add . && git commit -m "release prepare" && git push
 git checkout master
 #mvn versions:set -DgenerateBackupPoms=false
 git merge develop
 git add . && git commit -m "release prepare" && git push
-mvn --batch-mode -s /Users/rm/Eclipse/m2/fms/settings.xml release:prepare release:perform
+mvn --batch-mode -DreleaseVersion=$RV -DdevelopmentVersion=$DV -s /Users/rm/Eclipse/m2/fms/settings.xml release:prepare release:perform
 git checkout develop
+git merge master
+
