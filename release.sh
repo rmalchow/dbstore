c678c5e5deb1a00427f65c02df72059ae2235c1f
ruben malchow <ruben.malchow@gmail.com>
2015-03-19 08:40:46 +0800

    release prepare

--- release.sh
+++ release.sh
@@ -7,7 +7,8 @@ git checkout master
 #mvn versions:set -DgenerateBackupPoms=false
 git merge develop
 git add . && git commit -m "release prepare" && git push
-mvn --batch-mode -DreleaseVersion=$RV -DdevelopmentVersion=$DV -s /Users/rm/Eclipse/m2/fms/settings.xml release:prepare release:perform
+mvn --batch-mode -Dtag=dbstore-$RV release:prepare -DreleaseVersion=$RV -DdevelopmentVersion=$DV -s /Users/rm/Eclipse/m2/fms/settings.xml 
+mvn --batch-mode -s /Users/rm/Eclipse/m2/fms/settings.xml release:perform
 git checkout develop
 git merge master
 
