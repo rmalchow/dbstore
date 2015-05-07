894f8eaa1b05509bf76375826a32301f4ac89b62
rm <ruben.malchow@gmail.com>
2015-05-07 14:32:31 +0800

    default naming strategy is set

--- dbstore-mongo/src/main/java/com/cinefms/dbstore/utils/mongo/MongoDataStore.java
+++ dbstore-mongo/src/main/java/com/cinefms/dbstore/utils/mongo/MongoDataStore.java
@@ -2,10 +2,11 @@ package com.cinefms.dbstore.utils.mongo;
 
 import com.cinefms.dbstore.api.DataStore;
 import com.cinefms.dbstore.utils.mongo.util.CollectionNamingStrategy;
+import com.cinefms.dbstore.utils.mongo.util.SimpleCollectionNamingStrategy;
 
 public class MongoDataStore extends AMongoDataStore implements DataStore {
 
-	private CollectionNamingStrategy collectionNamingStrategy;
+	private CollectionNamingStrategy collectionNamingStrategy = new SimpleCollectionNamingStrategy();
 	
 	public String getCollectionName(Class<?> clazz) {
 		return collectionNamingStrategy.getCollectionName(clazz);
