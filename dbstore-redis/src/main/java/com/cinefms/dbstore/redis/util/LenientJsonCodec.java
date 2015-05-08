43ecc7a22698d535eb8145947e764de4b58ebcbb
rm <ruben.malchow@gmail.com>
2015-05-08 17:12:39 +0800

    release prepare

--- dbstore-redis/src/main/java/com/cinefms/dbstore/redis/util/LenientJsonCodec.java
+++ dbstore-redis/src/main/java/com/cinefms/dbstore/redis/util/LenientJsonCodec.java
@@ -2,6 +2,8 @@ package com.cinefms.dbstore.redis.util;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.redisson.codec.JsonJacksonCodec;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -10,6 +12,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class LenientJsonCodec extends JsonJacksonCodec {
 
+	private static Log log = LogFactory.getLog(LenientJsonCodec.class);
+	
 	
 	private ObjectMapper mapper;
 	
@@ -17,15 +21,14 @@ public class LenientJsonCodec extends JsonJacksonCodec {
 	protected void init() {
 		if(mapper==null) {
 			mapper = new ObjectMapper();
-			
 		}
 		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
 		mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,false);
+		log.info("### INITIALIZED LENIENT JSON CODEC FOR REDIS ... ");
+		
 		super.init(mapper);
 	}
-
-
 
 	public ObjectMapper getMapper() {
 		return mapper;
