937813debbf46bede37ff45dfbb5c04228652e03
rm <ruben.malchow@gmail.com>
2015-05-08 18:13:12 +0800

    release prepare

--- dbstore-redis/src/main/java/com/cinefms/dbstore/redis/util/LenientJsonCodec.java
+++ dbstore-redis/src/main/java/com/cinefms/dbstore/redis/util/LenientJsonCodec.java
@@ -1,155 +1,32 @@
 package com.cinefms.dbstore.redis.util;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.redisson.codec.RedissonCodec;
+import org.redisson.codec.JsonJacksonCodec;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectMapper.DefaultTypeResolverBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
 
-public class LenientJsonCodec implements RedissonCodec {
+public class LenientJsonCodec extends JsonJacksonCodec {
 
 	private static Log log = LogFactory.getLog(LenientJsonCodec.class);
 	
-	private ObjectMapper mapper = new ObjectMapper();
-
+	
+	public LenientJsonCodec() {
+		super();
+		log.info("### LENIENT JSON CODEC INSTANTIATED .... ");
+	}
 	
 	@PostConstruct
-    public void init() {
-        TypeResolverBuilder<?> typer = new DefaultTypeResolverBuilder(DefaultTyping.NON_FINAL);
-        typer.init(JsonTypeInfo.Id.CLASS, null);
-        typer.inclusion(JsonTypeInfo.As.PROPERTY);
-        mapper.setDefaultTyping(typer);
-
-        // type info inclusion
-        TypeResolverBuilder<?> mapTyper = new DefaultTypeResolverBuilder(DefaultTyping.NON_FINAL) {
-            public boolean useForType(JavaType t)
-            {
-                switch (_appliesFor) {
-                case NON_CONCRETE_AND_ARRAYS:
-                    while (t.isArrayType()) {
-                        t = t.getContentType();
-                    }
-                    // fall through
-                case OBJECT_AND_NON_CONCRETE:
-                    return (t.getRawClass() == Object.class) || !t.isConcrete();
-                case NON_FINAL:
-                    while (t.isArrayType()) {
-                        t = t.getContentType();
-                    }
-                    // to fix problem with wrong long to int conversion
-                    if (t.getRawClass() == Long.class) {
-                        return true;
-                    }
-                    return !t.isFinal(); // includes Object.class
-                default:
-                //case JAVA_LANG_OBJECT:
-                    return (t.getRawClass() == Object.class);
-                }
-            }
-        };
-        mapTyper.init(JsonTypeInfo.Id.CLASS, null);
-        mapTyper.inclusion(JsonTypeInfo.As.PROPERTY);
-
-        mapper.setDefaultTyping(mapTyper);
+	protected void init(ObjectMapper mapper) {
 		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
 		mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,false);
-
-        mapper.setSerializationInclusion(Include.NON_NULL);
-        mapper.setVisibilityChecker(mapper.getSerializationConfig().getDefaultVisibilityChecker()
-                                            .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                                            .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-                                            .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-                                            .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(SerializationFeature.WRITE_BIGDECIMAL_AS_PLAIN, true);
-        mapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
-
-	}
-
-    @Override
-    public Object decodeKey(ByteBuffer bytes) {
-        return new String(bytes.array(), bytes.arrayOffset() + bytes.position(), bytes.limit(), Charset.forName("ASCII"));
-    }
-
-    @Override
-    public Object decodeValue(ByteBuffer bytes) {
-        return decode(bytes);
-    }
-
-    private Object decode(ByteBuffer bytes) {
-        try {
-            return mapper.readValue(bytes.array(), bytes.arrayOffset() + bytes.position(), bytes.limit(), Object.class);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    @Override
-    public byte[] encodeKey(Object key) {
-        return key.toString().getBytes(Charset.forName("ASCII"));
-    }
-
-    @Override
-    public byte[] encodeValue(Object value) {
-        try {
-            return mapper.writeValueAsBytes(value);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    @Override
-    public byte[] encodeMapValue(Object value) {
-        try {
-            return mapper.writeValueAsBytes(value);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    @Override
-    public byte[] encodeMapKey(Object key) {
-        return encodeMapValue(key);
-    }
-
-    @Override
-    public Object decodeMapValue(ByteBuffer bytes) {
-        try {
-            return mapper.readValue(bytes.array(), bytes.arrayOffset() + bytes.position(), bytes.limit(), Object.class);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    @Override
-    public Object decodeMapKey(ByteBuffer bytes) {
-        return decodeMapValue(bytes);
-    }	
-
-	public ObjectMapper getMapper() {
-		return mapper;
-	}
-
-
-
-	public void setMapper(ObjectMapper mapper) {
-		this.mapper = mapper;
+		log.info("### INITIALIZED LENIENT JSON CODEC FOR REDIS ... ");
+		super.init(mapper);
 	}
 
 }
