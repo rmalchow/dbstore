f596e0abe8fc073c3931c6aed7d6fd0767b3be3e
rm <ruben.malchow@gmail.com>
2015-05-29 17:04:50 +0800

    release prepare

--- release.sh
+++ release.sh
@@ -3,8 +3,7 @@ git checkout master
 #mvn versions:set -DgenerateBackupPoms=false
 git merge develop
 git add . && git commit -m "release prepare" && git push
-mvn --batch-mode -s /Users/rm/Eclipse/m2/fms/settings.xml release:prepare
-mvn --batch-mode -s /Users/rm/Eclipse/m2/fms/settings.xml release:perform
+mvn --batch-mode -s /Users/rm/Eclipse/m2/skjlls/settings-home.xml -Dresume=false release:prepare release:perform
 git checkout develop
 git merge master
 
