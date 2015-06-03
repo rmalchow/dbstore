8d7e11d6d2aa5e3ee812e3f4859b1b4254576102
rm <ruben.malchow@gmail.com>
2015-06-03 11:04:25 +0800

    release prepare

--- dbstore-mongo/src/main/java/com/cinefms/dbstore/utils/mongo/MongoService.java
+++ dbstore-mongo/src/main/java/com/cinefms/dbstore/utils/mongo/MongoService.java
@@ -1,18 +1,21 @@
 package com.cinefms.dbstore.utils.mongo;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 
 public class MongoService {
 
-	private String host="127.0.0.1";
 	private int port=27017;
 	private String dbName;
+	private String hosts;
 
 	private MongoClient client;
 	
@@ -20,11 +23,25 @@ public class MongoService {
 	
 	public MongoClient getClient() throws UnknownHostException {
 		if(client == null) {
-			client = new MongoClient(getHost(),getPort());
+			client = new MongoClient(getServers());
 		}
 		return client;
 	}
 	
+	private List<ServerAddress> getServers() throws UnknownHostException {
+		List<ServerAddress> out = new ArrayList<ServerAddress>();
+		for(String h : getHosts().split(",")) {
+			String[] x = h.split(":");
+			String host = x[0];
+			int port = this.port; 
+			if(x.length>1) {
+				port = Integer.parseInt(x[1]);
+			}
+			out.add(new ServerAddress(host, port));
+		}
+		return out;
+	}
+
 	public DB getDb() throws UnknownHostException {
 		return getDb(getDbName());
 	}
@@ -39,14 +56,6 @@ public class MongoService {
 		return out;
 	}
 	
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
 	public String getDbName() {
 		return dbName==null?"tmp_"+Math.round(Math.random()*2933d):dbName;
 	}
@@ -55,13 +64,13 @@ public class MongoService {
 		this.dbName = dbName;
 	}
 
-	public int getPort() {
-		return port;
+	public String getHosts() {
+		return hosts;
 	}
 
-	public void setPort(int port) {
-		this.port = port;
+	public void setHosts(String hosts) {
+		this.hosts = hosts;
 	}
-	
+
 	
 }
