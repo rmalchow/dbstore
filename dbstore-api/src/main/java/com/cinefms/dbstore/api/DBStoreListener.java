22c4af8f9155487e29dcff1aa793390fe7128132
rm <ruben.malchow@gmail.com>
2015-12-22 19:40:20 +0800

    release prepare

--- dbstore-api/src/main/java/com/cinefms/dbstore/api/DBStoreListener.java
+++ dbstore-api/src/main/java/com/cinefms/dbstore/api/DBStoreListener.java
@@ -1,15 +1,15 @@
 package com.cinefms.dbstore.api;
 
-public interface DBStoreListener {
+public interface DBStoreListener<T extends DBStoreEntity> {
 	
 	public boolean supports(Class<? extends DBStoreEntity> clazz);
 
-	public void beforeSave(String db, DBStoreEntity o);
-	public void beforeDelete(String db, DBStoreEntity o);
+	public void beforeSave(String db, T o);
+	public void beforeDelete(String db, T o);
 
-	public void created(String db, DBStoreEntity o);
-	public void updated(String db, DBStoreEntity oldEntity, DBStoreEntity newEntity);
-	public void deleted(String db, DBStoreEntity o);
+	public void created(String db, T o);
+	public void updated(String db, T oldEntity, T newEntity);
+	public void deleted(String db, T o);
 	
 
 }
--- dbstore-api/src/main/java/com/cinefms/dbstore/api/impl/DBStoreListenerAdapter.java
+++ dbstore-api/src/main/java/com/cinefms/dbstore/api/impl/DBStoreListenerAdapter.java
@@ -1,9 +1,11 @@
 package com.cinefms.dbstore.api.impl;
 
+import java.lang.reflect.ParameterizedType;
+
 import com.cinefms.dbstore.api.DBStoreEntity;
 import com.cinefms.dbstore.api.DBStoreListener;
 
-public abstract class DBStoreListenerAdapter<T extends DBStoreEntity> implements DBStoreListener {
+public abstract class DBStoreListenerAdapter<T extends DBStoreEntity> implements DBStoreListener<T> {
 	
 	private Class<T> clazz;
 	
@@ -11,23 +13,32 @@ public abstract class DBStoreListenerAdapter<T extends DBStoreEntity> implements
 		this.clazz = clazz;
 	}
 
-	public boolean supports(Class<? extends DBStoreEntity> clazz) {
-		return clazz==this.clazz;
+	@SuppressWarnings("unchecked")
+	public Class<T> getClazz() {
+		if(clazz==null) {
+			ParameterizedType superclass = (ParameterizedType)getClass().getGenericSuperclass();
+			clazz = (Class<T>) superclass.getActualTypeArguments()[0];
+		}
+		return clazz;
 	}
 
-	public void beforeSave(String db, DBStoreEntity o) {
+	public boolean supports() {
+		return clazz==getClazz();
 	}
 
-	public void updated(String db, DBStoreEntity oldEntity, DBStoreEntity newEntity) {
+	public void beforeSave(String db, T o) {
 	}
 
-	public void beforeDelete(String db, DBStoreEntity o) {
+	public void updated(String db, T oldEntity, T newEntity) {
 	}
 
-	public void deleted(String db, DBStoreEntity o) {
+	public void beforeDelete(String db, T o) {
 	}
 
-	public void created(String db, DBStoreEntity o) {
+	public void deleted(String db, T o) {
+	}
+
+	public void created(String db, T o) {
 	}
 	
 
