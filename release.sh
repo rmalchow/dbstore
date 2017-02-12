3ddab7de7544d1fd83515ad242a16c8d31e12d12
ruben malchow <ruben.malchow@gmail.com>
2017-02-12 15:24:27 +0100

    [ci-skip] release prepare 0.7.11

--- release.sh
+++
@@ -1,9 +0,0 @@
-git add . && git commit -m "release prepare" && git push
-git checkout master
-#mvn versions:set -DgenerateBackupPoms=false
-git merge develop
-git add . && git commit -m "release prepare" && git push
-mvn --batch-mode -s /Users/rm/Eclipse/m2/skjlls/settings-home.xml -Dresume=false release:prepare release:perform
-git checkout develop
-git merge master
-
